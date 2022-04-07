2ec6f30868162c63fb4b4fd1ffe91177d1308c31
huwarr <mikhalmatova@gmail.com>
2022-04-08 00:15:00 +0300

    minor fix in eval script

--- eval.sh
+++ eval.sh
@@ -10,7 +10,7 @@ TMSPAN=$3
 PRE_PATH=$4
 PRETRAIN_PATH=$5
 
-BERT_CONFIG="--roberta_model ${PRETRAIN_PATH}"
+BERT_CONFIG="--encoder ${PRETRAIN_PATH}"
 
 echo "Use tag_mspan model..."
 MODEL_CONFIG="--gcn_steps 3 --use_gcn --tag_mspan"
@@ -19,7 +19,7 @@ echo "Starting evaluation..."
 TEST_CONFIG="--eval_batch_size 32 --pre_path ${PRE_PATH} --data_mode dev --dump_path ${DUMP_PATH} \
              --inf_path ${DATA_PATH}"
 
-python roberta_predict.py \
+python3 roberta_predict.py \
     ${TEST_CONFIG} \
     ${BERT_CONFIG} \
     ${MODEL_CONFIG}
