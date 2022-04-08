854c4e6b80f922bc85c2d64b9a27c47900a97739
huwarr <mikhalmatova@gmail.com>
2022-04-08 22:42:51 +0300

    minor fixes in 'setup' script

--- tools/setup.sh
+++ tools/setup.sh
@@ -9,24 +9,22 @@ unzip drop_dataset.zip
 
 # Download GenBERT pretrained model
 cd drop_dataset
-mkdir genbert
-cd genbert
 pip install --upgrade --no-cache-dir gdown
 gdown --folder https://drive.google.com/drive/folders/1-KmhWF4Jex4gyuz1J2VANd1ycmtsggQ3
-cd ../..
+cd ..
 
 # Tag based multi-span extraction -- NumNet+ v2
 if [ ${TMSPAN} = tag_mspan ]; then
     # Train
-    sh train.sh 345 5e-4 1.5e-5 5e-5 0.01 tag_mspan
+    sh train.sh 345 5e-4 1.5e-5 5e-5 0.01 tag_mspan drop_dataset/
     # Eval
-    sh eval.sh drop_dataset/drop_dataset_dev.json prediction.json tag_mspan
+    sh eval.sh drop_dataset/drop_dataset_dev.json prediction.json tag_mspan numnet_plus_345_LR_5e-4_BLR_1.5e-5_WD_5e-5_BWD_0.01tag_mspan/checkpoint_best.pt drop_dataset/encoder/
     python3 drop_eval.py --gold_path drop_dataset/drop_dataset_dev.json --prediction_path prediction.json
 # Simple multi-span extraction -- NumNet+
 else
     # Train
-    sh train.sh 345 5e-4 1.5e-5 5e-5 0.01
+    sh train.sh 345 5e-4 1.5e-5 5e-5 0.01 no drop_dataset/
     # Eval
-    sh eval.sh drop_dataset/drop_dataset_dev.json prediction.json
+    sh eval.sh drop_dataset/drop_dataset_dev.json prediction.json no numnet_plus_345_LR_5e-4_BLR_1.5e-5_WD_5e-5_BWD_0.01/checkpoint_best.pt drop_dataset/encoder/
     python3 drop_eval.py --gold_path drop_dataset/drop_dataset_dev.json --prediction_path prediction.json
 fi
